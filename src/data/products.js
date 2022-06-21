322f863cfa82c02a794d99a44a0b444f7ab040a2
jqnorg <artikdev14@gmail.com>
2022-06-20 22:52:02 -0300

    correcting corrections

---
+++ src/data/products.js
@@ -0,0 +1,21 @@
+export const products = [{
+    "id": "1",
+    "title": "Lionel Messi Signed Jersey",
+    "description": "Barcelona 2015 Champions League Jersey signed by Lionel Messi" ,
+    "price": "1600",
+    "image": "https://res.cloudinary.com/dptvtsotj/image/upload/v1655347885/messi2015_gbmloz.png"
+},
+{
+    "id": "2",
+    "title": "Michael Jordan Signed Jersey",
+    "description": "Bulls's last championship jersey signed by Michael Jordan" ,
+    "price": "1300",
+    "image": "https://res.cloudinary.com/dptvtsotj/image/upload/v1655347885/jordanbulls_zykm38.png"
+},
+{
+    "id": "3",
+    "title": "Benzema Signed Jersey",
+    "description": "Real Madrid's 2022 Champions League jersey signed by Benzema",
+    "price": "1250",
+    "image": "https://res.cloudinary.com/dptvtsotj/image/upload/v1655347884/benzema2022_akjfqv.png"
+}]
